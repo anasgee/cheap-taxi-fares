4eb9408cc3273a0194a0446bd8ad6feb48e85ebe
Anas Raza <anasraza6096@gmail.com>
2024-06-12 19:57:00 +0500

    Added files to stagging

---
+++ js/index.js
@@ -0,0 +1,17 @@
+// to show and hide airport field
+
+
+const airport = document.getElementById("isAirportBooking");
+const airportField = document.getElementById("flightNumber");
+airport.addEventListener('click',()=>{
+airportField.style.display = airport.checked ? "block" : "none";
+});
+
+// date.innerHTML = `Hi this is date`;
+
+
+
+
+
+// Jquery
+
